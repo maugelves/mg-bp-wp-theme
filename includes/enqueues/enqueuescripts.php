bea7e2dd641cef4468664c28518622c79f79611a
maugelves <mgelves@gmail.com>
2017-10-16 09:37:45 +0200

    Added LiveReload

--- includes/enqueues/enqueuescripts.php
+++ includes/enqueues/enqueuescripts.php
@@ -3,16 +3,11 @@
  * Register the JS scripts
  */
 function site_scripts() {
-	/*
-	* Para discriminar por la versión comprimida o descomprimida agrego una constante en
-	* el wp-config.php llamada 'WP_DEVELOPMENTMODE'. En caso de que sea TRUE estamos en entorno
-	* de 'Desarrollo', en caso de que sea FALSE o bien que no exista estamos en el entorno de 'Producción'
-	*
-	* Teniendo en cuenta el valor de la constante genero la extensión del fichero '.js' para 'Desarrollo'
-	* y 'min.js' para 'Producción'
-	*/
-  	$jsExtension = (defined('WP_DEVELOPMENTMODE') && WP_DEVELOPMENTMODE )?'.js':'.min.js';
-  
+
+	// Load LiveReload library for development purposes
+	if( defined('WP_DEBUG') && true === WP_DEBUG )
+		wp_enqueue_script('livereload', get_bloginfo( 'template_url' ) . '/js/lib/livereload.js', array(), '', false );
+
 	/* Registrar y encolar aquí los ficheros Javascripts. Ejemplo:
 	wp_register_script('commonjs', get_bloginfo('template_url') . '/js/common' . $jsExtension, array('jquery'),'',true);
 	wp_enqueue_script('contactojs');
