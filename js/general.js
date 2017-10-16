ce0b325a4a615a19de7fa4b78f7015e9cd8f6125
maugelves <mgelves@gmail.com>
2017-10-16 11:30:29 +0200

    Added javascript functions to hamburgers icons

--- js/general.js
+++ js/general.js
@@ -1,9 +1,20 @@
 jQuery(document).ready(function($){
 
 	general = {
-		init: function(){
+		addEventListeners: function() {
 
-		}
+            // Add Click Listener to the Hamburguer
+            u('.hamburger').on('click', function(){
+                u(this).toggleClass('is-active');
+                u('.main-navigation').toggleClass('main-navigation--active');
+            })
+
+        },
+        init: function(){
+
+            general.addEventListeners();
+
+        }
 	}
 
 	// Initialize the general object
