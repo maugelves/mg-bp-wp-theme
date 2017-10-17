4647ac6e32d31223ae7f36e694c9cff5efecc9a8
maugelves <mgelves@gmail.com>
2017-10-17 17:34:09 +0200

    Added collapsible menu

--- js/general.js
+++ js/general.js
@@ -7,7 +7,21 @@
             // Add Click Listener to the Hamburguer
             u('.hamburger').on('click', function(){
                 u(this).toggleClass('is-active');
-                u('.main-navigation').toggleClass('main-navigation--active');
+                
+                // Collapse Menu
+                var section = document.querySelector('.main-navigation');
+                var isOpened = section.getAttribute('data-opened') === 'true';
+
+                if( isOpened ) {
+
+                    general.collapseSection(section);
+                    section.setAttribute('data-opened', 'false');
+
+                } else {
+
+                    general.expandSection(section);
+                }
+
             })
 
         },
@@ -15,6 +29,51 @@
 
             general.addEventListeners();
 
+        },
+        collapseSection: function (element) {
+            // get the height of the element's inner content, regardless of its actual size
+            var sectionHeight = element.scrollHeight;
+
+            // temporarily disable all css transitions
+            var elementTransition = element.style.transition;
+            element.style.transition = '';
+
+            // on the next frame (as soon as the previous style change has taken effect),
+            // explicitly set the element's height to its current pixel height, so we
+            // aren't transitioning out of 'auto'
+            requestAnimationFrame(function() {
+                element.style.height = sectionHeight + 'px';
+                element.style.transition = elementTransition;
+
+                // on the next frame (as soon as the previous style change has taken effect),
+                // have the element transition to height: 0
+                requestAnimationFrame(function() {
+                    element.style.height = 0 + 'px';
+                });
+            });
+
+            // mark the section as "currently closed"
+            element.setAttribute('data-opened', 'false');
+        },
+        expandSection: function(element) {
+
+            // get the height of the element's inner content, regardless of its actual size
+            var sectionHeight = element.scrollHeight;
+
+            // have the element transition to the height of its inner content
+            element.style.height = sectionHeight + 'px';
+
+            // when the next css transition finishes (which should be the one we just triggered)
+            element.addEventListener('transitionend', function(e) {
+                // remove this event listener so it only gets triggered once
+                element.removeEventListener('transitionend', arguments.callee);
+
+                // remove "height" from the element's inline styles, so it can return to its initial value
+                //element.style.height = null;
+            });
+
+            // mark the section as "currently opened"
+            element.setAttribute('data-opened', 'true');
         }
         
     };
